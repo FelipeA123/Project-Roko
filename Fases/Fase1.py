966fdbb7c0b873c132e1c70b9a5ff495d9528be4
Marcos-mega <marcos.ferreiramega0@gmail.com>
2025-05-24 17:09:03 -0300

    Teste

---
+++ Fases/Fase1.py
@@ -0,0 +1,8 @@
+from Mecanicas.skill import *
+from Mecanicas.dinheiro import *
+from Mecanicas.eficiencia import *
+from Mecanicas.progresso_jogo import Progresso_Jogo
+
+class Fase1:
+    def rodar_jogo(self):
+        while
